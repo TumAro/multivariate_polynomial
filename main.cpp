e25759295694c9e986f0d8654c754452cbc9228d
tumaro <contact.tumins@gmail.com>
2025-09-09 18:33:03 +0530

    implemented particle multiplication

--- main.cpp
+++ main.cpp
@@ -8,13 +8,22 @@ int main() {
 
     Atom poly[3] = {a, b, c};
 
-    float sol = 0;
-    for (int i = 0; i<3 ;i++) {
-        sol = sol + poly[i].eval(2);
-    };
+    // float sol = 0;
+    // for (int i = 0; i<3 ;i++) {
+    //     sol = sol + poly[i].eval(2);
+    // };
 
-    std::cout << sol<<std::endl;
+    // std::cout << sol<<std::endl;
 
-    
+    Particle p1, p2;
+    p1.atoms[0] = Atom(25,'x', 1);
+    p2.atoms[0] = Atom(3,'x', 1);
+
+    p1.count = 1;
+    p2.count = 1;
+
+    Particle p3 = p1 * p2;
+
+    std::cout << p3.eval({{'x', 1}, {'y', 1}}) <<std::endl;
     return 0;
 }
