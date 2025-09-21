c82109574c203bbf5d058c45c6ff537cd4e4a41f
TumAro <tuminspersonal@gmail.com>
2025-09-21 17:38:34 +0530

    added todo

--- tests/polynom_div.cpp
+++ tests/polynom_div.cpp
@@ -23,11 +23,12 @@ int main() {
     P2.addParticle(p2); 
     P2.addParticle(p3);
 
-    Polynomial P = P2 % P1;
+    // Polynomial P = P2 % P1;
 
-    // P1.print();
-    // P2.print();
-    P.print();
+    // P.print();
+
+    // Polynomial P = P2 % P1;
+    // P.print();
 
     // --------------------
     // Case 1: x²y + 3x
@@ -79,6 +80,37 @@ int main() {
     // A.print();
     // B.print();
 
+    // 3x^2yz + 3y^2zx + 3xyz^2
+    Polynomial T1;
+    Particle a1, a2, a3;
+    a1.addAtom(Atom(3, 'x', 2));
+    a1.addAtom(Atom(1, 'y', 1));
+    a1.addAtom(Atom(1, 'z', 1));
+
+    a2.addAtom(Atom(1, 'x', 1));
+    a2.addAtom(Atom(3, 'y', 2));
+    a2.addAtom(Atom(1, 'z', 1));
+
+    a3.addAtom(Atom(1, 'x', 1));
+    a3.addAtom(Atom(1, 'y', 1));
+    a3.addAtom(Atom(3, 'z', 2));
+
+    T1.addParticle(a2);
+    T1.addParticle(a3);
+    T1.addParticle(a1);
+
+    T1.print();
+
+    Polynomial T2;
+    Particle b1;
+    b1.addAtom(Atom(1, 'x', 1));  // x
+    b1.addAtom(Atom(1, 'y', 1));  // y²
+    T2.addParticle(b1);
+
+    T2.print();
+
+    Polynomial P = T1 / T2;
+    P.print();
 
     return 0;
 }
