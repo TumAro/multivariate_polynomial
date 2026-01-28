f13b81258a32ef07df107c832bb4a72ea3776c52
tumaro <contact.tumins@gmail.com>
2026-01-28 17:04:06 +0530

    organised the files

--- old_src/linalg.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "matrix.h"
-
-// Function declarations
-Polynomial determinant(const PolynomialMatrix& M);
-Polynomial cofactor(const PolynomialMatrix& M, int row, int col);
--- old_src/module.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "polynomial.h"  // Includes Atom, Particle, and Polynomial
-#include "matrix.h"      // PolynomialMatrix class
-#include "IOhandler.h"   // Array conversion utilities
-#include "linalg.h"      // Linear algebra operations (determinant, cofactor)
-#include "parser.h"      // String parsing utilities
